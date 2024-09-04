e900c0074d44a91ef28f30912aab87e742fe5ae1
Said Rafili <therafilisaid@gmail.com>
2024-09-04 10:33:31 +0200

    Implemented the IUserInteraction interface and ConsoleUserInteraction class that will responsible for interacting with user. Implemented the ReadFileNameFromUser() method that gets the file name from user and correctly handles the null and empty input.

---
+++ Ideas.cs
@@ -0,0 +1,30 @@
+﻿/*
+
+Design:
+
+1) IUserInteraction for handling input/output operations with user.
+ 
+* ConsoleUserInteraction class will implement its methods.
+
+2) IGameDataRepository for reading from a file the data
+
+* GameDataFileRepository will implement its methods.
+
+3) INameValidator for validating the names 
+
+* FileNameValidator class will implement its methods
+ - check for the null
+ - check for the empty name
+
+4) IContentValidator for checking if some file meets the certain format criterias
+
+* JsonContentValidator will check if the file's content is a valid json content.
+
+Ideas:
+
+1) Reading from a Json file.
+
+* If the Json string is null or empty we may throw ArgumentNullException or JsonException
+* If the Json string is not in a valid format, we may throw JsonException 
+
+*/
--- Program.cs
+++ Program.cs
@@ -3,6 +3,74 @@ public class Program
 {
     public static void Main(string[] args)
     {
-
+        IUserInteraction userInteraction = new ConsoleUserInteraction();
+        
+        GameDataParserApp App = new GameDataParserApp(userInteraction);
+        App.Run();
     }
 }
+
+public class GameDataParserApp
+{
+    private readonly IUserInteraction _userInteraction;
+
+    public GameDataParserApp(IUserInteraction userInteraction)
+    {
+        _userInteraction = userInteraction;
+    }
+
+    public void Run()
+    {
+        string fileName = _userInteraction.ReadFileNameFromUser();
+    }
+}
+
+public interface IUserInteraction
+{
+    string ReadFileNameFromUser();
+    void ShowMessage(string message);
+    void ShowMessageWithoutNewLine(string message);
+}
+
+public class ConsoleUserInteraction : IUserInteraction
+{
+    public string ReadFileNameFromUser()
+    {
+        string fileName = "";
+        while (true)
+        {
+            ShowMessage("Enter the name of the file you want to read: ");
+            fileName = Console.ReadLine();
+
+            if (fileName is null)
+            {
+                ShowMessage("File name cannot be null.\n");
+            }
+            else if (fileName == "")
+            {
+                ShowMessage("File name cannot be empty.\n");
+            }
+            else
+            {
+                if (!File.Exists(fileName))
+                    ShowMessage("File not found.\n");
+                else
+                    return fileName;
+            }
+            
+           
+
+        }
+    }
+
+    public void ShowMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public void ShowMessageWithoutNewLine(string message)
+    {
+        Console.Write(message);
+    }
+}
+
